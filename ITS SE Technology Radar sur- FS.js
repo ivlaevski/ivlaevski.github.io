e7c318ae5443cab7de9c766b7d1a91447a72ef9c
Ivan Vlaevski <ivlaevski@gmail.com>
2021-11-29 17:00:05 +0200

    new version

--- ITS SE Technology Radar sur- FS.js
+++ ITS SE Technology Radar sur- FS.js
@@ -3,7 +3,7 @@ new TechRadar.Blip("Microsoft Visual Studio", adopt, false, "<p><span style=\\\"
 ]);
 
 techniquesQuadrant.add([
-new TechRadar.Blip("Agile Scrum Methodology", adopt, true, "<p>Based on <a href=\\\"https://www.scaledagileframework.com/\\\">SAFe</a> our Agile Scrum Methodology is a knowledge base of tested and integrated principles, practices and competencies for achieving business agility using Lean, Agile, and DevOps.</p>"),
+new TechRadar.Blip("Agile Methodology", adopt, true, "<p>Based on <a href=\\\"https://www.scaledagileframework.com/\\\">SAFe</a> our Agile Methodology is a knowledge base of tested and integrated principles, practices and competencies for achieving business agility using Lean, Agile, and DevOps.</p>"),
 new TechRadar.Blip("Managed Cloud Environments", adopt, true, "<p><strong>One Platform</strong>, <strong>Cloud Next </strong>and <strong>Country Hosting </strong>- Managed Services and Cloud Environments hosted in Azure&nbsp;are recognized as the trusted partner in the delivery of cloud-based and managed services solutions. These solutions are designed to realize customers’ operational excellence goals.</p>"),
 new TechRadar.Blip("Software Delivery Guardrails", adopt, true, "<p>Empowering development teams to find, fix and prevent vulnerabilities related to source code, open source libraries, secret management and cloud configuration</p>"),
 new TechRadar.Blip("Strong Security Policy", adopt, false, "<p>We meet the the security requirements set out by the <strong>Global Information Security Policies</strong> enforcing management directions and support for information security in accordance with business requirements and relevant laws and regulations.</p>"),
@@ -11,7 +11,7 @@ new TechRadar.Blip("Strong Security Policy", adopt, false, "<p>We meet the the s
 
 platformsQuadrant.add([
 new TechRadar.Blip("MS Teams", adopt, false, "<p>Microsoft Teams is a proprietary business communication platform developed by Microsoft, as part of the Microsoft 365 family of products. Teams primarily competes with the similar service Slack, offering workspace chat and videoconferencing, file storage, and application integration.</p>"),
-new TechRadar.Blip("SOFY", trial, true, "<p>SOFY Suite enables you to operationalize data analytics and use it at the right time for the right purpose. The predefined applications of the SOFY Suite are based on our modern self-service BI, survey and workflow engine and KPMG's may years of experience in the areas of auditing and consulting.</p>"),
+new TechRadar.Blip("SOFY", adopt, true, "<p>SOFY Suite enables you to operationalize data analytics and use it at the right time for the right purpose. The predefined applications of the SOFY Suite are based on our modern self-service BI, survey and workflow engine and KPMG's may years of experience in the areas of auditing and consulting.</p>"),
 ]);
 
 languageFramework.add([
--- ITS SE Technology Radar sur- IA.js
+++ ITS SE Technology Radar sur- IA.js
@@ -4,8 +4,7 @@ new TechRadar.Blip("Microsoft Visual Studio", adopt, false, "<p><span style=\\\"
 ]);
 
 techniquesQuadrant.add([
-new TechRadar.Blip("TypeScript", adopt, false, "<p><span style=\\\"background-color:#ffffff; color:#444444; font-size:13px\\\"><a href=\\\"https://www.typescriptlang.org/\\\">TypeScript</a>&nbsp;is a programming language developed and maintained by Microsoft. It is a strict syntactical superset of JavaScript and adds optional static typing to the language. TypeScript is designed for the development of large applications and transcompiles to JavaScript.</span></p>"),
-new TechRadar.Blip("Agile Scrum Methodology", adopt, true, "<p>Based on <a href=\\\"https://www.scaledagileframework.com/\\\">SAFe</a> our Agile Scrum Methodology is a knowledge base of tested and integrated principles, practices and competencies for achieving business agility using Lean, Agile, and DevOps.</p>"),
+new TechRadar.Blip("Agile Methodology", adopt, true, "<p>Based on <a href=\\\"https://www.scaledagileframework.com/\\\">SAFe</a> our Agile Methodology is a knowledge base of tested and integrated principles, practices and competencies for achieving business agility using Lean, Agile, and DevOps.</p>"),
 new TechRadar.Blip("Software Delivery Guardrails", adopt, true, "<p>Empowering development teams to find, fix and prevent vulnerabilities related to source code, open source libraries, secret management and cloud configuration</p>"),
 new TechRadar.Blip("Strong Security Policy", adopt, false, "<p>We meet the the security requirements set out by the <strong>Global Information Security Policies</strong> enforcing management directions and support for information security in accordance with business requirements and relevant laws and regulations.</p>"),
 ]);
@@ -15,5 +14,6 @@ new TechRadar.Blip("MS Teams", adopt, false, "<p>Microsoft Teams is a proprietar
 ]);
 
 languageFramework.add([
+new TechRadar.Blip("TypeScript", adopt, false, "<p><span style=\\\"background-color:#ffffff; color:#444444; font-size:13px\\\"><a href=\\\"https://www.typescriptlang.org/\\\">TypeScript</a>&nbsp;is a programming language developed and maintained by Microsoft. It is a strict syntactical superset of JavaScript and adds optional static typing to the language. TypeScript is designed for the development of large applications and transcompiles to JavaScript.</span></p>"),
 ]);
 
